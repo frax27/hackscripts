e8bc4f52abd6c0c89b2d87ad1b226026c849e259
Luciano Kumaru <shinjox27@outlook.com>
2023-10-03 11:37:42 -0400

    Port Scanner
    Created from Linux Basics for Hackers Chapter 8!

---
+++ Alohascan.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# This script is designed to find systems with a certain port open
+echo "Enter the starting IP address to scan... :"
+read FIRSTIP
+echo "Enter the last octet of the last IP :"
+read LastOctetIP
+echo "Enter the port number you want to scan for :"
+read port
+nmap -sT $FIRSTIP-$LastOctetIP -p $port >/dev/null -oG Alohascan
+cat Alohascan | grep open > Alohascan2
+cat Alohascan2

